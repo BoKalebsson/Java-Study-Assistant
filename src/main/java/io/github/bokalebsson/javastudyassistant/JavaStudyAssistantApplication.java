8ad1898542bc7241ce250c8cb08195ed1cb477cc
BoKalebsson <bo_kalebsson@hotmail.com>
2026-01-03 11:18:35 +0100

    Implemented initial Spring Boot setup with OpenAI integration.

---
+++ src/main/java/io/github/bokalebsson/javastudyassistant/JavaStudyAssistantApplication.java
@@ -0,0 +1,13 @@
+package io.github.bokalebsson.javastudyassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStudyAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaStudyAssistantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/bokalebsson/javastudyassistant/JavaStudyAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bokalebsson.javastudyassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStudyAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
