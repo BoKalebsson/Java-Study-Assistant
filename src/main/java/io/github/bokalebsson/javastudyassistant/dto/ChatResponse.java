013d88e6682eaf9bd58da6a1498780959b65bd9d
BoKalebsson <bo_kalebsson@hotmail.com>
2026-01-03 12:04:47 +0100

    Implemented chat response DTO.

---
+++ src/main/java/io/github/bokalebsson/javastudyassistant/dto/ChatResponse.java
@@ -0,0 +1,11 @@
+package io.github.bokalebsson.javastudyassistant.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ChatResponse {
+
+    private String answer;
+}
