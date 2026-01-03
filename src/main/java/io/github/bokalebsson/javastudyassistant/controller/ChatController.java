a8197ca70e1f351f6e5d637404f7a57576730eec
BoKalebsson <bo_kalebsson@hotmail.com>
2026-01-03 15:38:14 +0100

    Implemented CORS configuration for localhost frontend.

--- src/main/java/io/github/bokalebsson/javastudyassistant/controller/ChatController.java
+++ src/main/java/io/github/bokalebsson/javastudyassistant/controller/ChatController.java
@@ -4,11 +4,9 @@ import io.github.bokalebsson.javastudyassistant.dto.ChatRequest;
 import io.github.bokalebsson.javastudyassistant.dto.ChatResponse;
 import io.github.bokalebsson.javastudyassistant.service.ChatService;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/chat")
 public class ChatController {
